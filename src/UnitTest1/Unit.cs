35869c9a86839cd953e3247276bf4b96e910045b
Лаврухин Максим <lavruxin.max@yandex.ru>
2025-03-08 15:26:58 +0300

    UnitTestAdded

---
+++ src/UnitTest1/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using ExamApp;
+
+namespace UnitTest1
+{
+    [Fact]
+    public class UnitTest1
+    {
+        Graph graph = new Graph();
+
+        graph.InitializeGraph(); 
+
+        Assert.NotNull(graph.AdjacencyMatrix);
+        Assert.True(graph.AdjacencyMatrix[0, 1]);
+
+        Assert.False(graph.AdjacencyMatrix[0, 2]);
+
+        Assert.Equal(graph.AdjacencyMatrix[0, 1], graph.AdjacencyMatrix[1, 0]);
+    }
+}
