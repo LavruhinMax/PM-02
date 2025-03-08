6e937156e0d0b3ff0473704c306b50c593e7ac77
Лаврухин Максим <lavruxin.max@yandex.ru>
2025-03-08 13:58:04 +0300

    Program

---
+++ src/ExamApp/ExamApp/Graph.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp
+{
+    class Graph
+    {
+        public bool[,] AdjacencyMatrix;
+        public double[,] WeightMatrix;
+        public static int n = 9;
+
+        public void InitializeGraph()
+        {
+            // Матрица смежности графа
+            AdjacencyMatrix = new bool[9, 9] {
+            { false,  true, false, false,  true, false, false, false, false },
+            {  true, false,  true, false, false, false, false, false, false },
+            { false,  true, false,  true, false, false, false,  true, false },
+            { false, false,  true, false,  true, false,  true, false, false },
+            {  true, false, false,  true, false,  true, false, false, false },
+            { false, false, false, false,  true, false,  true, false,  true },
+            { false, false, false,  true, false,  true, false,  true, false },
+            { false, false,  true, false, false, false,  true, false,  true },
+            { false, false, false, false, false,  true, false,  true, false }
+            };
+
+            WeightMatrix = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (AdjacencyMatrix[i, j])
+                        WeightMatrix[i, j] = 0;
+                    else
+                        WeightMatrix[i, j] = double.MaxValue; // Отсутствующее ребро
+                }
+            }
+        }
+
+        // Ввод ребер
+        public void InputWeights()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++) 
+                {
+                    if (!AdjacencyMatrix[i, j]) continue; 
+
+                    double distance;
+                    while (true)
+                    {
+                        Console.WriteLine($"Введите расстояние между вершинами {i + 1} и {j + 1}: ");
+                        if (double.TryParse(Console.ReadLine(), out distance) && distance > 0) // Только положительные значения
+                            break;
+                        Console.WriteLine("Ошибка! Введите положительное число.");
+                    }
+                    WeightMatrix[i, j] = distance;
+                    WeightMatrix[j, i] = distance;
+                }
+            }
+        }
+
+        // Поиск кратчайшего пути
+        public double FindShortestPath(int startVertex, int endVertex)
+        {
+            double[] distances = Dijkstra(WeightMatrix, startVertex);
+            return distances[endVertex];
+        }
+
+        // Алгоритм Дейкстры
+        private static double[] Dijkstra(double[,] a, int v0)
+        {
+            double[] dist = new double[n];
+            bool[] vis = new bool[n];
+            int unvis = n;
+            int v;
+
+            for (int i = 0; i < n; i++)
+                dist[i] = Double.MaxValue;
+            dist[v0] = 0.0;
+
+            while (unvis > 0)
+            {
+                v = -1;
+                for (int i = 0; i < n; i++)
+                {
+                    if (vis[i])
+                        continue;
+                    if ((v == -1) || (dist[v] > dist[i]))
+                        v = i;
+                }
+                vis[v] = true;
+                unvis--;
+                for (int i = 0; i < n; i++)
+                {
+                    if (dist[i] > dist[v] + a[v, i])
+                        dist[i] = dist[v] + a[v, i];
+                }
+            }
+            return dist;
+        }
+    }
+}
---
+++ src/ExamApp/ExamApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExamApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph cityGraph = new Graph();
+            cityGraph.InitializeGraph(); // Инициализация графа
+            cityGraph.InputWeights(); // Ввод весов ребер
+
+            Console.WriteLine("\n-- Поиск кратчайшего пути и расчет расхода топлива --\n");
+
+            // Ввод расхода топлива
+            Console.WriteLine("Введите расход топлива (л/100 км):");
+            double fuelConsumptionPer100Km = double.Parse(Console.ReadLine());
+
+            // Ввод начальной и конечной точек
+            Console.WriteLine("Введите стартовую точку:");
+            int startPoint = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конечную точку:");
+            int endPoint = int.Parse(Console.ReadLine());
+
+            // Поиск кратчайшего пути
+            double shortestDistance = cityGraph.FindShortestPath(startPoint - 1, endPoint - 1);
+
+            // Расчет расхода топлива
+            double totalFuelConsumption = CalculateFuelConsumption(shortestDistance, fuelConsumptionPer100Km);
+
+            // Вывод результатов
+            Console.WriteLine($"\nРасстояние от точки {startPoint} до точки {endPoint}: {shortestDistance} км");
+            Console.WriteLine($"Израсходовано топлива: {totalFuelConsumption} л");
+
+            Console.Read();
+        }
+
+        //Метод нахождения расхода топлива
+        private static double CalculateFuelConsumption(double distance, double consumption)
+        {
+            return (distance * consumption) / 100;
+        }
+    }
+}
